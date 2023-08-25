bc26860f6ae9542657f6b47bd155541c0d4dfc12
valadez-14 <avaladez1@students.avc.edu>
2023-08-25 13:35:35 -0700

    ass1.5

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 def main():
-    """
+   
     ##################################################
-    Comlete your code here
-    Use m_perc and f_perc for your results
+   # Comlete your code here
+   #Use m_perc and f_perc for your results
     ##################################################
-    """
+     numMales = input("Enter Number of males registered in the class:\n")
+     numFemales = input("Enter Number of males registered in the class:\n")
+     numMales = int(numMales)
+     numFemales =  int(numFemales)
+     total = numMales + numFemales
+     m_perc = numMales/total * 100
+     f_perc = numFemales/total * 100
 
-    """
+     print(f'The total number of students:\t\t\t\t{total}')
+     print(f'The number of males and females:\t\t\t{numMales}\t {numFemales}')
+     print(f'The percentage of males and females:\t\t\t{m_perc:.2f}%\t {f_perc:.2f}%')
+
+
+
+    
+
     ########################################
     # Do not delete the return statement
     ########################################
-    """
-    return m_perc, f_perc
+    
+     return m_perc, f_perc
 
 
 if __name__ == '__main__':
